fe59f36350b427535405161ac3e1284a5625bda2
zzyfd <zzyfromdream@gmail.com>
2021-10-18 13:14:43 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,4 +21,4 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yuexy/yuexy.github.io.git master:gh-pages
+git push -f git@github.com:zzfd/homepage master:gh-pages
