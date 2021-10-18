f056bf40ae5fc3bb834c36fec66f6de99effdfd2
张兆阳 <SENSETIME\zhangzhaoyang@hk0014000243l.domain.sensetime.com>
2021-10-18 13:25:58 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -21,4 +21,4 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:zzyfd/homepage.git
+git push -f https://github.com/zzyfd/homepage.git
